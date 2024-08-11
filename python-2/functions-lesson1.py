e7146729aaee58991c775853f26ba29d744d2e03
jakecohen-96 <yakop.96@gmail.com>
2024-08-11 19:56:28 +0300

    excercise in lesson 1 completed

--- python-2/functions-lesson1.py
+++ python-2/functions-lesson1.py
@@ -1,6 +1,22 @@
-def hello(greeting, name):
-    if greeting == 'hello':
-        print(f"goodbye" "{name}")
-    else:  
-        print(f"{greeting} {name}") 
-hello("jimmmy", "hello")
+# def hello(greeting, name):
+#     if greeting == 'hello':
+#         print(f"goodbye" "{name}")
+#     else:  
+#         print(f"{greeting} {name}") 
+# hello("jimmmy", "hello")
+
+# def hello(greeting, name='jimmy'):
+#     if greeting == 'hello':
+#         print(f"goodbye {name}")
+#     else:  
+#         print(f"{greeting} {name}") 
+# hello("ahoy!", "steve")
+
+
+def calculation(a, b):
+    addition = a + b
+    subtraction = a - b
+    return addition, subtraction
+
+res = calculation(40,10)
+print(res)
