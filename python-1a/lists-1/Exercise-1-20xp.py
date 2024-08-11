3d9424797c9f3fa744d5bcc5528eb4258029b90a
jakecohen-96 <yakop.96@gmail.com>
2024-08-11 19:09:36 +0300

    second commit

---
+++ python-1a/lists-1/Exercise-1-20xp.py
@@ -0,0 +1,28 @@
+# excersice number 1
+
+# my_fav_numbers = {5,27,96,1901}
+# my_fav_numbers.add(29)
+# my_fav_numbers.remove(1901)
+
+# friend_fav_numbers = {20,55,69,2024}
+
+# my_fav_numbers.update(friend_fav_numbers)
+
+# print(my_fav_numbers)
+
+# second 
+
+# first_tuple = (2,5,4,5)
+# second_tuple = (7,44,32,1)
+
+# combined = first_tuple + second_tuple
+
+# print(combined)
+
+# third
+
+basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+removed = basket.pop(0)
+removed = basket.pop(-1)
+basket.append('kiwi')
+print(basket)
