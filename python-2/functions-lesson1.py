050fcc7c4f30fee3293ebb47d55e1acdba22791c
jakecohen-96 <yakop.96@gmail.com>
2024-08-11 21:42:42 +0300

    learning lambda reduce filter and map

--- python-2/functions-lesson1.py
+++ python-2/functions-lesson1.py
@@ -13,10 +13,28 @@
 # hello("ahoy!", "steve")
 
 
-def calculation(a, b):
-    addition = a + b
-    subtraction = a - b
-    return addition, subtraction
+# def calculation(a, b):
+#     addition = a + b
+#     subtraction = a - b
+#     return addition, subtraction
 
-res = calculation(40,10)
-print(res)
+# res = calculation(40,10)
+# print(res)
+
+# def create(first, last):
+#     first = first.capitalize()
+#     last = last.capitalize()
+#     return 'hi' + ' ' + first + ' ' + last
+# full = create('jake', 'cohen')
+# print(full)
+
+from functools import reduce
+
+nums = [3,5,4,8,10,22,26,38,44,55]
+
+even = list(map(lambda n: n**2, nums))
+
+sum = reduce(lambda a,b, : a+b, nums)
+
+print(even)
+print(sum)
