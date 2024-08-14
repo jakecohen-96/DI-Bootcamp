75b5c432a10ffac64ceed429bfe370476c2965c1
jakecohen-96 <yakop.96@gmail.com>
2024-08-14 19:02:24 +0300

    checking who won  stage 1

--- python-2/mini-project/tic-tac-toe.py
+++ python-2/mini-project/tic-tac-toe.py
@@ -25,3 +25,20 @@ def player_input(player):
             if row >= 0 and row < 3 and col >= 0 and col < 3 and board[col][row] == " ":
                 board[col][row] = player
                 break
+            else:
+                print("Wrong move try again")
+
+# check who won 
+def check_win():
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] ! = " ":
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i] != " ":
+            return board[0][i]
+        
+    if board[0][0] == board[1][1] == board[2][2] != " ":
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != " ":
+        return board[0][2]
+
+
