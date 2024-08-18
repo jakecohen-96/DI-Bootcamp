8c85b797b1f514b6b16183a24c8a62151398ef65
jakecohen-96 <yakop.96@gmail.com>
2024-08-18 18:56:26 +0300

    first exercise

--- python-2/OOP/oop1.py
+++ python-2/OOP/oop1.py
@@ -1,27 +1,50 @@
-class dog():
-    def __init__(self, name, age, color, noise):
-        self.name = name
-        self.age = age
-        self.color = color
-        self.noise = noise
+# class dog():
+#     def __init__(self, name, age, color, noise):
+#         self.name = name
+#         self.age = age
+#         self.color = color
+#         self.noise = noise
     
-    def bark(self):
-        return f"{self.name} goes {self.noise} at night"
+#     def bark(self):
+#         return f"{self.name} goes {self.noise} at night"
     
-    def eat(self, food):
-        return f"{self.name} eats {food} ayyyy"
+#     def eat(self, food):
+#         return f"{self.name} eats {food} ayyyy"
     
-    def change_color(self, new_color):
-        self.color = new_color
-        print( f"{self.name} color changed to {new_color}")
+#     def change_color(self, new_color):
+#         self.color = new_color
+#         print( f"{self.name} color changed to {new_color}")
 
-first_dog = dog('rex', 5, 'black', 'bark')
-second_dog = dog('bobby', 4, 'brown', 'oink')
-third_dog = dog('damn', 9, 'pink', 'waaaa')
+# first_dog = dog('rex', 5, 'black', 'bark')
+# second_dog = dog('bobby', 4, 'brown', 'oink')
+# third_dog = dog('damn', 9, 'pink', 'waaaa')
 
-print(first_dog.eat('chicken'))
-print(second_dog.eat('tuna'))
+# print(first_dog.eat('chicken'))
+# print(second_dog.eat('tuna'))
 
-print(f"{first_dog.name} is getting older")
-print(f"He was {first_dog.color} but with age")
-first_dog.change_color('grey')
+# print(f"{first_dog.name} is getting older")
+# print(f"He was {first_dog.color} but with age")
+# first_dog.change_color('grey')
+
+# class Computer():
+
+#     def description(self, name):
+#         """
+#         This is a totally useless function
+#         """
+#         print("I am a computer, my name is", name)
+#         #Analyse the line below
+#         print(self)
+
+# mac_computer = Computer()
+# mac_computer.brand = "Apple"
+# print(mac_computer.brand)
+
+# dell_computer = Computer()
+
+# Computer.description(dell_computer, "Mark")
+# # IS THE SAME AS:
+# dell_computer.description("Mark")
+
+class BankAccount()
+    def 
---
+++ python-2/OOP/oop2.py
@@ -0,0 +1,29 @@
+# class Circle:
+#     color = "red"
+
+# class NewCircle(Circle):
+#     color = "blue"
+
+# nc = Circle
+# print(nc.color)
+
+
+class Circle:
+    def __init__(self, diameter):
+      self.diameter = diameter
+
+    def grow(self, factor=2):
+        """grows the circle's diameter by factor"""
+        self.diameter = self.diameter * factor
+
+class NewCircle(Circle):
+    def grow(self, factor=2):
+        """grows the area by factor..."""
+        self.diameter = (self.diameter * factor * 2)
+
+nc = NewCircle(1)
+print(nc.diameter)
+
+nc.grow()
+
+print(nc.diameter)
