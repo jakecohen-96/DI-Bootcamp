56056814cd6b98d5a5b33e7646b965d7f96703fd
jakecohen-96 <yakop.96@gmail.com>
2024-08-22 19:31:03 +0300

    trying to work with jsons

---
+++ python-2/IO-Json/index.py
@@ -0,0 +1,45 @@
+import json
+
+data = {
+    "firstName": "Jane",
+    "lastName": "Doe",
+    "hobbies": ["running", "sky diving", "singing"],
+    "age": 35,
+    "children": [
+        {
+            "firstName": "Summer",
+            "age": 6
+        },
+        {
+            "firstName": "Morty",
+            "age": 8
+        }
+    ]
+}
+
+json_file = "file.json"
+
+with open(json_file, 'w') as file_obj:
+    json.dump(data, file_obj, indent = 2, sort_keys=True)
+
+with open(json_file, 'r') as file_obj:
+    family = json.load(file_obj)
+
+children = data.get("children", [])
+for i, child in enumerate(children):
+    if child == 'Summer':
+        favorite_color = 'Yellow'
+    elif child == 'Morty':
+        favorite_color = 'Blue'
+    else:
+        favorite_color = 'Unknown'
+    children[i] = {"name": child, "favorite_color": favorite_color}
+    
+data["children"] = children
+
+print(data)
+
+with open(json_file, 'w') as file_obj:
+    json.dump(data, file_obj, indent = 2, sort_keys=True)
+
+    
