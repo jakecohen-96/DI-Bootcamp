5c7e8687a6bdde8ceb55fcf74e4755077bb541de
jakecohen-96 <yakop.96@gmail.com>
2024-08-22 19:16:57 +0300

    testing making json files

---
+++ python-2/IO-Json/test.py
@@ -0,0 +1,11 @@
+import json
+
+my_family = {
+    "parents":['Beth', 'Jerry'],
+    "children":['Summer', 'Morty']
+}
+
+json_file = "my_file.json"
+
+with open(json_file, 'w') as file_obj:
+    json.dump(my_family, file_obj)
