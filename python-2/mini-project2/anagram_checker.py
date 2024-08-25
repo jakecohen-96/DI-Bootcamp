45e791cdb00845e81109e347ea295994df0ee3c7
jakecohen-96 <yakop.96@gmail.com>
2024-08-25 18:17:43 +0300

    first commit

---
+++ python-2/mini-project2/anagram_checker.py
@@ -0,0 +1,12 @@
+class AnagramChecker:
+    def __init__(self) -> None:
+        pass
+
+    def is_valid_word(word):
+        pass
+
+    def get_anagrams(word):
+        pass
+
+    def is_anagram(word1, word2):
+        pass
