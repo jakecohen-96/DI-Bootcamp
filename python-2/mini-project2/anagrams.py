d00cb5ce8c40c56cf174c96793547270d4e5d5a0
jakecohen-96 <yakop.96@gmail.com>
2024-08-25 18:43:55 +0300

    menu done

--- python-2/mini-project2/anagrams.py
+++ python-2/mini-project2/anagrams.py
@@ -1,2 +1,19 @@
-import anagram_checker
+from anagram_checker import AnagramChecker
+
+def MainMenu():
+    while True:
+        print('Welcome to my Anagram Checker!')
+        print('1. Enter Word')
+        print('2. Exit')
+
+        choice = input('Please choose an option')
+
+        if choice == "1":
+            pass
+            handle_input()
+        elif choice == "2":
+            print('Thanks for playing!')
+            break
+        else:
+            print('Please enter a valid option')    
 
