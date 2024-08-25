6e6d86772a8b558a3e115e51e9c4314d410aa62c
jakecohen-96 <yakop.96@gmail.com>
2024-08-25 21:19:31 +0300

    changed a lot of stuff just so it can actually play

--- python-2/mini-project2_rock_paper_scissors/game.py
+++ python-2/mini-project2_rock_paper_scissors/game.py
@@ -1,37 +1,40 @@
 import random
 
-class play():
+class play:
     def get_user_item(self):
         while True:
-            user_choice = input('Please select (R)ock, (P)aper, or (S)cissors: ')
+            user_choice = input('Please select (R)ock, (P)aper, or (S)cissors: ').strip().upper()
             if user_choice in ["R", "S", "P"]:
                 if user_choice == "R":
-                    return 'Rock'
+                    return 'rock'
                 elif user_choice == "S":
-                    return 'Scissors'
+                    return 'scissors'
                 elif user_choice == "P":
-                    return 'Paper'
-                else:
-                    print('Please enter a valid option')
+                    return 'paper'
+            else:
+                print('Please enter a valid option')
 
     def get_computer_item(self):
-        def get_computer_item(self):
-            choices = ['rock', 'paper', 'scissors']
-            computer_choice = random.choice(choices)
-            return computer_choice
-    
-    def get_game_result(self, user_choice: str, computer_choice: str) -> str:
-        if user_choice == computer_choice:
-            print('It is a draw!')
+        choices = ['rock', 'paper', 'scissors']
+        computer_choice = random.choice(choices)
+        return computer_choice 
 
-        if (user_choice == 'rock' and computer_choice == 'scissors') or (user_choice == 'paper' and computer_choice == 'rock') or (user_choice == 'scissors' and computer_choice == 'paper'):
-            return "You won! One point! for you" 
+    def get_game_result(self, user_item: str, computer_item: str) -> str:
+        if user_item == computer_item:
+            return "draw"
         
-        return "loss"
+        if user_item == "rock" and computer_item == "scissors":
+            return "win"
+        elif user_item == "paper" and computer_item == "rock":
+            return "win"
+        elif user_item == "scissors" and computer_item == "paper":
+            return "win"
+        else:
+            return "loss"
     
     def play(self):
         user_item = self.get_user_item()
-        computer_item = self.get_computer_item()
+        computer_item = self.get_computer_item()  # This should return a string, not a list
         result = self.get_game_result(user_item, computer_item)
-        print(f'You slected {user_item}, Computer chose {computer_item} Result: {result}')
+        print(f'You selected {user_item.capitalize()}, Computer chose {computer_item}. Result: {result}')
         return result
