42a3522c535c78d5c178b463de480ae4ce9b3d29
jakecohen-96 <yakop.96@gmail.com>
2024-08-25 18:30:36 +0300

    is valid and is anagram done

--- python-2/mini-project2/anagram_checker.py
+++ python-2/mini-project2/anagram_checker.py
@@ -3,11 +3,13 @@ class AnagramChecker:
         with open('wordlist.txt', 'r') as file:
             self.words = [line.strip().lower() for line in file]
 
-    def is_valid_word(word):
+    def is_valid_word(self, word: str) -> bool:
+        return word in self.words
         
 
-    def get_anagrams(word):
+    def get_anagrams(self, word):
         pass
 
-    def is_anagram(word1, word2):
-        pass
+    def is_anagram(self, word1: str, word2: str) -> bool:
+        return sorted(word1) == sorted(word2)
+        
