ac7880f2061807628e588a73604110929a98ba0c
jakecohen-96 <yakop.96@gmail.com>
2024-08-27 22:42:08 +0300

    step one actually done

--- python-2/oop2/inheritance/excercise-xp/excercise5.py
+++ python-2/oop2/inheritance/excercise-xp/excercise5.py
@@ -2,7 +2,9 @@ from excercise4 import Family
 
 class TheIncredibles(Family):
     def use_power(self, name):
-        for members in self.members:
-            if members['name'] == name:
-                if members['age'] >= 18:
-                    print(f'{member['name']}'s power is: {member['power']}')
+        for member in self.members:
+            if member['name'] == name:
+                if member['age'] >= 18:
+                    print(f"{member['name']}'s power is: {member['power']}")
+                else:
+                    raise Exception(f"{member['name']} is not over 18 years old no power yet!")
