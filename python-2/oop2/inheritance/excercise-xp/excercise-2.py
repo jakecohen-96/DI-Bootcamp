d37356c7fb0aa0fd889b82900d3ff036a971fb5f
jakecohen-96 <yakop.96@gmail.com>
2024-08-27 21:23:03 +0300

    bark and speed done

--- python-2/oop2/inheritance/excercise-xp/excercise-2.py
+++ python-2/oop2/inheritance/excercise-xp/excercise-2.py
@@ -1,3 +1,15 @@
 class dog():
     def __init__(self, name, age, weight):
-        pass
+        name.self = name
+        age.self = age
+        weight.self = weight
+
+    def bark(self):
+        return f'{self.name} is barking'
+    
+    def run_speed(self):
+        speed = (self.weight/self.age*10)
+        return speed
+    
+    def fight(self, other_dog):
+        
