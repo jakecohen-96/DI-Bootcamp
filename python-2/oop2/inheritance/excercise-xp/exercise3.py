61b6cf331e5f65f82dd62f2ce9255b42c246242c
jakecohen-96 <yakop.96@gmail.com>
2024-08-27 21:45:43 +0300

    steps 1 to 2 done

--- python-2/oop2/inheritance/excercise-xp/exercise3.py
+++ python-2/oop2/inheritance/excercise-xp/exercise3.py
@@ -1,3 +1,6 @@
-from excercise2 import dog
+from excercise2 import Dog
 
-class PetDog(dog)
+class PetDog(Dog):
+    def __init__(self, name, age, weight):
+        super().__init__(name, age, weight)
+        self.trained = False
