6455b199d4ae55410c53832bbc82bc6a485e308d
jakecohen-96 <yakop.96@gmail.com>
2024-08-27 22:24:10 +0300

    done

--- python-2/oop2/inheritance/excercise-xp/excercise4.py
+++ python-2/oop2/inheritance/excercise-xp/excercise4.py
@@ -3,8 +3,8 @@ class Family():
         self.members = []
         self.last_name = last_name
 
-    def Born(self, **kwargs):
-        self.members.append(**kwargs) += 1
+    def born(self, **kwargs):
+        self.members.append(kwargs)
         print(f"Congratulations to the {self.last_name} family on the birth of {kwargs['name']}!")
 
     def is_18(self, name):
@@ -17,3 +17,16 @@ class Family():
         print(f"The {self.last_name} family consists of:")
         for member in self.members:
             print(member)
+
+my_family = Family("George", "Smith")
+
+my_family.members = [
+    {'name': 'Michael', 'age': 35, 'gender': 'Male', 'is_child': False},
+    {'name': 'Sarah', 'age': 32, 'gender': 'Female', 'is_child': False}
+]
+
+print(my_family.family_presentation())
+print(my_family.born(name="Emily", age=0, gender="Female", is_child=True))
+print(my_family.family_presentation())
+print(my_family.is_18('Michael'))
+print(my_family.is_18("Emily"))
