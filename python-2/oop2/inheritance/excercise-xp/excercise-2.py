f4758e639375f3b6e39169317b9894a61dbe4cb4
jakecohen-96 <yakop.96@gmail.com>
2024-08-27 21:37:02 +0300

    done

--- python-2/oop2/inheritance/excercise-xp/excercise-2.py
+++ python-2/oop2/inheritance/excercise-xp/excercise-2.py
@@ -1,8 +1,8 @@
 class dog():
     def __init__(self, name, age, weight):
-        name.self = name
-        age.self = age
-        weight.self = weight
+        self.name = name
+        self.age = age
+        self.weight = weight
 
     def bark(self):
         return f'{self.name} is barking'
@@ -17,8 +17,18 @@ class dog():
         other_dog_strength = other_dog.run_speed() * other_dog.weight 
 
         if self_strength > other_dog_strength:
-            print('That is a good boy!')
+            return f'{self.name} is a good boy!! {other_dog} should get better strength training'
         elif other_dog_strength > self_strength:
-            print('strength training tomorrow')
+            return f'{self.name} need strength training tomorrow! {other_dog} was simply better'
         else:
             print('Its a draw' )    
+
+dog1 = dog('Kinder', 4, 50)
+dog2 = dog('Moochie', 3, 32)
+dog3 = dog('Max', 2, 10)
+
+print(dog1.bark())
+print(dog2.bark())
+print(dog3.bark())
+
+print(dog1.fight(dog2))
