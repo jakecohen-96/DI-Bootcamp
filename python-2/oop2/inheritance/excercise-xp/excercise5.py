c793111cb3c644087d939d4433439e17813b6db4
jakecohen-96 <yakop.96@gmail.com>
2024-08-27 22:46:14 +0300

    done

--- python-2/oop2/inheritance/excercise-xp/excercise5.py
+++ python-2/oop2/inheritance/excercise-xp/excercise5.py
@@ -11,4 +11,16 @@ class TheIncredibles(Family):
                 
     def incredible_presentation(self):
         print("Here is our powerful family:")
-        Family().family_presentation()
+        super().family_presentation()
+    
+incredibles_family = TheIncredibles("Incredibles", 'Incredibles')
+incredibles_family.members = [
+    {'name': 'Michael', 'age': 35, 'gender': 'Male', 'is_child': False, 'power': 'fly', 'incredible_name': 'MikeFly'},
+    {'name': 'Sarah', 'age': 32, 'gender': 'Female', 'is_child': False, 'power': 'read minds', 'incredible_name': 'SuperMind'}
+]
+
+incredibles_family.incredible_presentation()
+
+incredibles_family.born(name='Baby Jack', age=0, gender='Male', is_child=True, power='Unknown Power', incredible_name='JackJack')
+
+incredibles_family.incredible_presentation()
