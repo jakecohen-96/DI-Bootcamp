5946366e25025cd7f0b66dabc19f67b688a90e77
jakecohen-96 <yakop.96@gmail.com>
2024-08-27 22:12:22 +0300

    steps 1 to 4

--- python-2/oop2/inheritance/excercise-xp/excercise4.py
+++ python-2/oop2/inheritance/excercise-xp/excercise4.py
@@ -3,6 +3,17 @@ class Family():
         self.members = []
         self.last_name = last_name
 
-    def born(self, **kwargs):
+    def Born(self, **kwargs):
         self.members.append(**kwargs) += 1
         print(f"Congratulations to the {self.last_name} family on the birth of {kwargs['name']}!")
+
+    def is_18(self, name):
+        for member in self.members:
+            if member['name'] == name:
+                return member['age'] >= 18
+        return False     
+
+    def family_presentation(self):
+        print(f"The {self.last_name} family consists of:")
+        for member in self.members:
+            print(member)
