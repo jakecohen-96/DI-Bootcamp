836537bf76a41ee7ba8b5645187ef57647d2dc10
jakecohen-96 <yakop.96@gmail.com>
2024-08-29 18:31:19 +0300

    done first step

--- python-2/oop2/inheritance/Exercises-XP-Gold/exercise-1.py
+++ python-2/oop2/inheritance/Exercises-XP-Gold/exercise-1.py
@@ -2,9 +2,15 @@ class BankAccount():
     def __init__(self, balance = 0):
         self.balance = balance
 
-    def deposit(self):
-        
+    def deposit(self, amount):
+        if amount <= 0:
+            raise ValueError("Please deposit an amount over 0")
+        else:
+           self.balance += amount 
 
-    def withdraw(self):
-        pass
+    def withdraw(self, amount):
+        if amount >= 0:
+            raise ValueError("Not enough cash!!")
+        else:
+           self.balance -= amount 
         
