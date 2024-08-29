ebbf0e89257095cbb2a66746d04c31944b6f9640
jakecohen-96 <yakop.96@gmail.com>
2024-08-29 18:36:50 +0300

    step 2 done

--- python-2/oop2/inheritance/Exercises-XP-Gold/exercise-1.py
+++ python-2/oop2/inheritance/Exercises-XP-Gold/exercise-1.py
@@ -14,3 +14,15 @@ class BankAccount():
         else:
            self.balance -= amount 
         
+
+class MinimumBalanceAccount(BankAccount):
+        def __init__(self, balance=0, minimum_balance=0):
+            super().__init__(balance)
+            self.minimum_balance = minimum_balance
+        
+        def withdraw(self, amount):
+            if self.balance - amount < self.minimum_balance:
+                raise ValueError("Withdrawal would go below the minimum balance")
+            super().withdraw(amount)
+
+
