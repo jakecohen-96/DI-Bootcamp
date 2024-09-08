12bae45e8d97b56c490ee13aea86229f1b9cf063
jakecohen-96 <yakop.96@gmail.com>
2024-09-08 20:17:05 +0300

    started enviroment

---
+++ SQL/python/new-example/new_example.py
@@ -0,0 +1,24 @@
+# CRUD (CREATE, READ, UPDATE, DELETE)
+
+# 1. create virtual enviroment
+# 2. activate virtual env
+# 3. install dependancies
+# 4. start coding
+#   a. write function to create table
+#   b. write function to insert data
+#   x. write a function READ
+#   x. write a function UPDATE
+#   x. write a function DELETE
+
+#   x. write a MAIN function to RUN
+
+# first name
+# last name
+# class name
+
+import sqlite3
+from tabulate import tabulate
+
+class StudentInfo():
+    def __init__(self):
+        pass
