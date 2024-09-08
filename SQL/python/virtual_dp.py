14b637c943bbbd9a6ae2a907ef58356fc0de165d
jakecohen-96 <yakop.96@gmail.com>
2024-09-08 19:39:21 +0300

    first commit

--- SQL/python/virtual_dp.py
+++ SQL/python/virtual_dp.py
@@ -9,13 +9,65 @@ connection = psycopg2.connect(host=HOSTNAME, user=USERNAME, password=PASSWORD, d
 
 cursor = connection.cursor()
 
-query = "SELECT * FROM actors LIMIT 7;"
+def create_table(table_name: str): 
+    """create new table with id, num"""
 
-cursor.execute(query)
+    query = f'''
+    create table {table_name}(
+        id serial primary key,
+        num integer not null
+    );
+    '''
+    cursor.execute(query) # execute the query
+    connection.commit() # make changes in the database
 
-results = cursor.fetchall()
+def insert_into_table(table_name: str, num_value: int):
 
-for row in results:
-    print(row)
+    query = f'''
+    insert into {table_name}(num)
+    values
+    ({num_value})
+    '''
+    cursor.execute(query) # execute the query
+    connection.commit() # make changes in the database
 
-connection.close()
+def select_all(table_name: str):
+
+    query = f'''
+    select * from {table_name};
+    '''
+
+    cursor.execute(query)
+    output = cursor.fetchall()
+    return output
+
+
+
+if __name__ == '__main__':
+    try:
+        connection = psycopg2.connect(
+            dbname = DB_NAME,
+            user = USER,
+            password = PASSWORD,
+            host = HOST,
+            port = PORT
+        )
+    except Exception as e:
+        print(f"Error: {e}")
+
+    # 2. Create a cursor object
+    cursor = connection.cursor()
+
+    table_name = 'new_table3'
+
+    create_table(table_name)
+
+    insert_into_table(table_name, 12312)
+    insert_into_table(table_name, 88888)
+
+    print(select_all(table_name))
+
+    # 4. Close database connection
+
+    connection.close()
+
