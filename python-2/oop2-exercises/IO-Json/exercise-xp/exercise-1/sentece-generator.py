42a976c205dd6e8e32d15c5d968098d3bec3b997
jakecohen-96 <yakop.96@gmail.com>
2024-09-10 20:02:45 +0300

    second done

--- python-2/oop2-exercises/IO-Json/exercise-xp/exercise-1/sentece-generator.py
+++ python-2/oop2-exercises/IO-Json/exercise-xp/exercise-1/sentece-generator.py
@@ -1,4 +1,12 @@
+import random
+
 def get_words_from_file():
     with open('sowpods.txt', 'r') as file:
         words = file.read().splitlines
     return words
+
+def get_random_sentence(length):
+    words = get_words_from_file()
+    random_words = random.sample(words, length)
+    sentence = ' '.join(random_words).lower()
+    return sentence
