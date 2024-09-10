39383581004db567dfee8450b36d009e540e363a
jakecohen-96 <yakop.96@gmail.com>
2024-09-10 20:56:30 +0300

    started work on editor, menu done

--- python-2/oop2-exercises/IO-Json-API/exercise-gold/exercise-1/menu_editor.py
+++ python-2/oop2-exercises/IO-Json-API/exercise-gold/exercise-1/menu_editor.py
@@ -1,8 +1,14 @@
+from menu_manager import MenuManager
+
 def load_manager():
-    pass
+    return MenuManager()
 
 def show_user_menu():
-    pass   
+    print("\nRestaurant Menu Manager")
+    print("1. Show Menu")
+    print("2. Add Item to Menu")
+    print("3. Remove Item from Menu")
+    print("4. Exit")   
 
 def add_item_to_menu():
     pass
