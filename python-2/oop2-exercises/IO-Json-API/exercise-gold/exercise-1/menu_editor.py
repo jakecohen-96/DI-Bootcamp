a9ebd59a5931558590b39903f8d4ba853ee0bbbb
jakecohen-96 <yakop.96@gmail.com>
2024-09-10 20:45:08 +0300

    starting

---
+++ python-2/oop2-exercises/IO-Json-API/exercise-gold/exercise-1/menu_editor.py
@@ -0,0 +1,15 @@
+def load_manager():
+    pass
+
+def show_user_menu():
+    pass   
+
+def add_item_to_menu():
+    pass
+
+def remove_item_from_menu():
+    pass
+
+def show_restaurant_menu():
+    pass        
+
---
+++ python-2/oop2-exercises/IO-Json-API/exercise-gold/exercise-1/menu_manager.py
@@ -0,0 +1,15 @@
+import json
+
+class MenuManager():
+    def __init__(self):
+      pass
+
+    def add_item(self, name, price):
+       pass
+
+    def remove_item(self, name):
+       pass
+
+    def save_to_file(self):
+       pass
+        
