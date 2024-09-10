a7f87da8f0e541b063893109237c168228099885
jakecohen-96 <yakop.96@gmail.com>
2024-09-10 20:52:22 +0300

    manager is done

--- python-2/oop2-exercises/IO-Json-API/exercise-gold/exercise-1/menu_manager.py
+++ python-2/oop2-exercises/IO-Json-API/exercise-gold/exercise-1/menu_manager.py
@@ -2,14 +2,21 @@ import json
 
 class MenuManager():
     def __init__(self):
-      pass
+      with open('restaurant_menu.json', 'r') as file:
+         self.menu = json.load(file)
 
     def add_item(self, name, price):
-       pass
+       new_item = {'Name:': name, 'Price:': price }
+       self.menu['items'].append(new_item)
 
     def remove_item(self, name):
-       pass
+        for item in self.menu['items']:
+            if item['name'].lower() == name.lower():
+                self.menu['items'].remove(item)
+                return True
+        return False
 
     def save_to_file(self):
-       pass
+        with open('restaurant_menu.json', 'w') as file:
+            json.dump(self.menu, file, indent=4)
         
