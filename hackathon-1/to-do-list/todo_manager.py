1b56e4ad89e02a48257880b77a619417a8b20e8c
jakecohen-96 <yakop.96@gmail.com>
2024-09-12 21:38:03 +0300

    main functions done, maybe more to follow?

--- hackathon-1/to-do-list/todo_manager.py
+++ hackathon-1/to-do-list/todo_manager.py
@@ -35,7 +35,14 @@ class ToDoList():
             print('Invalid option chosen')
 
     def edit_tasks(self, task_number, new_task):
-        pass
+        task_index = task_number - 1
+        if 0 <= task_index < len(self.tasks):
+            old_task = self.tasks[task_index]['task']
+            self.tasks[task_index]['task'] = new_task
+            self.tasks[task_index]['completed'] = False
+            print(f'The task "{old_task}" has been updated to "{new_task}". \nIf this task was completed, it has also been changed to "Incomplete"')
+        else:  
+            print('Invalid option chosen')
 
 
 if __name__ == "__main__":
@@ -59,4 +66,5 @@ if __name__ == "__main__":
     todo_list.complete_tasks(3)
     todo_list.view_tasks()
     todo_list.remove_tasks(3)
+    todo_list.edit_tasks(2, 'petting the dog')
     todo_list.view_tasks()
