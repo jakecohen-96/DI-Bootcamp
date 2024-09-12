5baab9cb44c0c3ed0c25c1a50453af828dd61991
jakecohen-96 <yakop.96@gmail.com>
2024-09-12 19:39:49 +0300

    first commit, files created (two for now), first functions for todo_manager file

---
+++ hackathon-1/to-do-list/todo_manager.py
@@ -0,0 +1,21 @@
+class ToDoList():
+    def __init__(self):
+        self.tasks = []
+
+    def view_task(self):
+        pass
+
+    def add_task(self, task):
+        pass        
+
+    def remove_task(self, task_number):
+        pass
+
+    def complete_task(self, task_number):
+        pass
+
+    def edit_task(self, task_number, new_task):
+        pass
+
+
+    
