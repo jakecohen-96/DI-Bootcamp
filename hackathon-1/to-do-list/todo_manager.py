3739cb37373d93452fd9493c45cfc6a3017eae54
jakecohen-96 <yakop.96@gmail.com>
2024-09-12 20:11:29 +0300

    complete function done, tests on the bottom from a temp list for now

--- hackathon-1/to-do-list/todo_manager.py
+++ hackathon-1/to-do-list/todo_manager.py
@@ -1,8 +1,12 @@
 class ToDoList():
     def __init__(self):
-        self.tasks = []
+        self.tasks = [
+            {"task": "Buy groceries", "completed": False},
+            {"task": "Read a book", "completed": False},
+            {"task": "Clean the house", "completed": False}
+            ]
 
-    def view_task(self):
+    def view_tasks(self):
         if not self.tasks:
             print('No items present in the list. Please add some first.')
         else:
@@ -10,17 +14,31 @@ class ToDoList():
                 status = '✔️' if task.get('completed') else '❌'
                 print(f'{i + 1}, {task['task']} - {status}')
 
-    def add_task(self, task):
+    def add_tasks(self, task):
         pass        
 
-    def remove_task(self, task_number):
+    def remove_tasks(self, task_number):
         pass
 
-    def complete_task(self, task_number):
-        pass
+    def complete_tasks(self, task_number):
+        if 0 <= task_number < len(self.tasks):
+            self.tasks[task_number]['completed'] = True
+            print(f'{self.tasks[task_number]['task']} completed!')
+        else:
+            print('Invalid option chosen')
 
     def edit_task(self, task_number, new_task):
         pass
 
 
-    
+if __name__ == "__main__":
+    todo_list = ToDoList()
+
+    print('tasks before complete: ')
+    todo_list.view_tasks()
+
+    todo_list.complete_tasks(1)
+    print('completed task 2')
+
+    print('tasks before complete: ')
+    todo_list.view_tasks()    
