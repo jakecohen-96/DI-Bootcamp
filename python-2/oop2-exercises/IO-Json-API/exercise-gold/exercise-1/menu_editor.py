049f232b74c7ffa61f15fbd050d6db7a25a6033a
jakecohen-96 <yakop.96@gmail.com>
2024-09-12 19:24:17 +0300

    comitting

--- python-2/oop2-exercises/IO-Json-API/exercise-gold/exercise-1/menu_editor.py
+++ python-2/oop2-exercises/IO-Json-API/exercise-gold/exercise-1/menu_editor.py
@@ -10,12 +10,24 @@ def show_user_menu():
     print("3. Remove Item from Menu")
     print("4. Exit")   
 
-def add_item_to_menu():
-    pass
+def add_item_to_menu(manager):
+    name = input('Please add item name: ')
+    price = float(input('Please add a price: '))
+    manager.add_item(name, price)
+    print('added!')
 
-def remove_item_from_menu():
-    pass
+def remove_item_from_menu(manager):
+    name = input('Please enter item name to delete: ')
+    if manager.remove_item(name):
+        print('dome')
+    else:
+        print('Item not found!')
 
-def show_restaurant_menu():
-    pass        
 
+def show_restaurant_menu(manager):
+     print("\nCurrent Menu:")
+     for item in manager.menu['items']:
+        print(f"{item['name']}: ${item['price']}")        
+
+def main():
+    manager = load_manager()
