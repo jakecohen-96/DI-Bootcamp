ecf13e18b2790281d16209ccf19e70028fb3bd65
jakecohen-96 <yakop.96@gmail.com>
2024-09-13 15:11:45 +0300

    fixed some bugs with incomplete and clear functions and semantics

--- hackathon-1/to-do-list/main.py
+++ hackathon-1/to-do-list/main.py
@@ -21,39 +21,47 @@ def main():
             todo_list.view_tasks()
 
         elif choice == "2":
-            task = input("Please enter a task to set up")
+            task = input("Please enter a task to set up: ")
             todo_list.add_tasks(task)
             todo_list.view_tasks()
+            todo_list.save_to_file()
 
         elif choice == "3":
             task_num = int(input("Please enter the number of the task you wish to delete: "))
             todo_list.remove_tasks(task_num)
             todo_list.view_tasks()
+            todo_list.save_to_file()
         
         elif choice == "4":
             task_comp = int(input("Please choose task you wish to mark completed: "))
             todo_list.complete_tasks(task_comp)
             todo_list.view_tasks()
+            todo_list.save_to_file()
 
         elif choice == "5":
             task_incomp = int(input('Please enter number of task you wish to mark incomplete: '))
             todo_list.incomplete_tasks(task_incomp)
             todo_list.view_tasks()
+            todo_list.save_to_file()
 
         elif choice == "6":
             task_edit_numb = int(input("Please enter number of task you wish to edit: "))
             task_edit = input("Please enter what you wish to change it to: ")
             todo_list.edit_tasks(task_edit_numb, task_edit)
             todo_list.view_tasks()
+            todo_list.save_to_file()
 
         elif choice == "7":
             todo_list.clear_all()
-            todo_list.view_tasks()
+            todo_list.save_to_file()
 
         elif choice == "8":
+            if todo_list.tasks:
+                print("Thank you for using the app! Here is your final list. Goodbye!")
+                todo_list.view_tasks()
+            else:
+                print("Thank you for using the app!")
             todo_list.save_to_file()
-            print("Thank you for using the app! Here is your final list. Goodbye!")
-            todo_list.view_tasks()
             break
 
         else:
