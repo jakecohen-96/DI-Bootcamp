6abd3aac242f68c57ba421f65cd16ffa40ee77b2
jakecohen-96 <yakop.96@gmail.com>
2024-09-14 20:50:28 +0300

    main functions of auth handler done

--- hackathon-1/to-do-list/auth_handle.py
+++ hackathon-1/to-do-list/auth_handle.py
@@ -3,17 +3,36 @@ import os
 
 class AuthManager():
     def __init__(self, filename='users.json'):
-        pass
+        self.filename = filename
+        self.users = self.load_users()
 
     def load_users(self):
-        pass
+        if os.path.exists(self.filename):
+            with open(self.filename, 'r') as file:
+                return json.load(file)
+        else:
+            return {}
 
     def save_user(self):
-        pass
+        with open(self.filename, 'w') as file:
+            json.dump(self.users, file)
 
     def register(self, username, password):
-        pass
+        if username in self.users:
+            print('User already exists!')
+            return False
+        else:
+            self.users[username] = password
+            self.save_user()
+            print('Registration succesful!')
+            return True
+        
 
     def log_in(self, username, password):
-        pass
+        if username in self.users and self.users[username] == password:
+            print('logged in!')
+            return True
+        else:
+            print('username or password entered is incorrect')
+            return False
 
