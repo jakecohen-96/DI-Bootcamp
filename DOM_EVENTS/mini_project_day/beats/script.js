dbd728bd9f3844c670f45611c00b8965c821c547
jakecohen-96 <yakop.96@gmail.com>
2024-09-28 19:16:14 +0300

    playsound functionb done i think

--- DOM_EVENTS/mini_project_day/beats/script.js
+++ DOM_EVENTS/mini_project_day/beats/script.js
@@ -4,5 +4,10 @@ function playSound(keycode) {
     const pad = document.querySelector(`.drum-pad[data-key="${keycode}"]`);
 
     if (!audio) return;
-    
+
+    audio.currentTime = 0;
+    audio.play();
+
+    pad.classList.add('active');
+    setTimeout(() => pad.classList.remove('active'), 100);
 }
