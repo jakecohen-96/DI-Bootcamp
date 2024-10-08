f1a53b298fdb103a98252876f957a01f7e372ff4
jakecohen-96 <yakop.96@gmail.com>
2024-10-08 19:29:03 +0300

    it works!! added randomizer variable that uses random function to randomize number from 1 to 83 (hence +1)

--- adv_js/mini_projects/star_wars/script.js
+++ adv_js/mini_projects/star_wars/script.js
@@ -1,15 +1,18 @@
 document.getElementById('fetch-button').addEventListener('click', function() {
     document.querySelector('.loading').style.display = 'block';  
+    
+    const randomId = Math.floor(Math.random() * 83) + 1;
 
-fetch("https://swapi.dev/api/people/1/")
+fetch(`https://swapi.dev/api/people/${randomId}/`)
     .then(response => response.json())
     .then(data => {
         document.querySelector('.loading').style.display = 'None'; 
+        
 
         document.getElementById('name').textContent = 'Name: ' + data.name;
-        document.getElementById('height').textContent = 'Height: ' + data.name;
-        document.getElementById('gender').textContent = 'Gender: ' + data.name;
-        document.getElementById('birth-year').textContent = 'Birth Year: ' + data.name;
+        document.getElementById('height').textContent = 'Height: ' + data.height;
+        document.getElementById('gender').textContent = 'Gender: ' + data.gender;
+        document.getElementById('birth-year').textContent = 'Birth Year: ' + data.birth_year;
         
         document.querySelector('.char-info').style.display = 'Block';
     })
@@ -19,4 +22,5 @@ fetch("https://swapi.dev/api/people/1/")
     });
 
 document.querySelector(".character-info").style.display = "none";
+
 });
