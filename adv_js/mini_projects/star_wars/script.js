3108226e2a34e2efea57d6ae59f19024c531a49d
jakecohen-96 <yakop.96@gmail.com>
2024-10-08 18:57:37 +0300

    added fetch to fetch elements from URL, added two "thens" one for the json and one to actually fetch and display data with DOM elements. also "loading" now goes away when it fetches the data (it is still not working but we are getting ther i hope)

--- adv_js/mini_projects/star_wars/script.js
+++ adv_js/mini_projects/star_wars/script.js
@@ -1,3 +1,14 @@
 document.getElementById('fetch-button').addEventListener('click', function() {
     document.querySelector('.loading').style.display = 'block';  
 });
+
+fetch("https://swapi.dev/api/people/1/")
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector('.loading').style.display = 'None'; 
+
+        document.getElementById('name').textContent = 'Name: ' + data.name;
+        document.getElementById('height').textContent = 'Height: ' + data.name;
+        document.getElementById('gender').textContent = 'Gender: ' + data.name;
+        document.getElementById('birth-year').textContent = 'Birth Year: ' + data.name;  
+    })
