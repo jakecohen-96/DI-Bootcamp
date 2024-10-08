e996e253b85a6286f9a88b9cd18dfc60ce4b89e0
jakecohen-96 <yakop.96@gmail.com>
2024-10-08 19:15:13 +0300

    fixed the loading not disappearing

--- adv_js/mini_projects/star_wars/script.js
+++ adv_js/mini_projects/star_wars/script.js
@@ -1,6 +1,5 @@
 document.getElementById('fetch-button').addEventListener('click', function() {
     document.querySelector('.loading').style.display = 'block';  
-});
 
 fetch("https://swapi.dev/api/people/1/")
     .then(response => response.json())
@@ -10,5 +9,14 @@ fetch("https://swapi.dev/api/people/1/")
         document.getElementById('name').textContent = 'Name: ' + data.name;
         document.getElementById('height').textContent = 'Height: ' + data.name;
         document.getElementById('gender').textContent = 'Gender: ' + data.name;
-        document.getElementById('birth-year').textContent = 'Birth Year: ' + data.name;  
+        document.getElementById('birth-year').textContent = 'Birth Year: ' + data.name;
+        
+        document.querySelector('.char-info').style.display = 'Block';
     })
+    .catch(error => {
+        document.querySelector('.loading').style.display = 'None';
+        alert('An error has occured');
+    });
+
+document.querySelector(".character-info").style.display = "none";
+});
