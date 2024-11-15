9114b04f475fd81c0b0246e9a5553e6cb748f981
jakecohen-96 <yakop.96@gmail.com>
2024-11-15 10:39:28 +0200

    done

--- week11/day2/Exercises-xp/exercise3/app.js
+++ week11/day2/Exercises-xp/exercise3/app.js
@@ -6,12 +6,13 @@ const booksRouter = require('./routes/books');
 // parsing to json
 app.use(express.json());
 
-
+// welcome message
 app.get('/', (req, res) => {
     res.send('Welcome to the books api.');
 });
 
-
+// using routes
+app.use('/api', booksRouter);
 
 app.listen(PORT, () => {
     console.log(`running on //localhost:${PORT}!!!!`);
