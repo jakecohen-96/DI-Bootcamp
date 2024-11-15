da051c29e2caaf465a16e331b7c64e818f35f24e
jakecohen-96 <yakop.96@gmail.com>
2024-11-15 11:15:17 +0200

    done

--- week13/day4/Exercises-xp/exercise1/server.js
+++ week13/day4/Exercises-xp/exercise1/server.js
@@ -1,10 +1,21 @@
+// Dependencies
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const postRoutes = require('./routes/postRoutes');
 
+// parsing
 app.use(express.json());
 
+// welcome message
+app.get('/', (req, res) => {
+    res.send('Welcome to blog api; use /posts for access to posts');
+});
+
+// using routes
+app.use('/api', postRoutes);
+
+// running
 app.listen(PORT, () => {
     console.log(`running on http://localhost:${PORT}`);
 });
-
