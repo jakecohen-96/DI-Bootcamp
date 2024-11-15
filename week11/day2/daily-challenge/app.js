f464f49c384991c36c4cff980c1f8c935b5261e0
jakecohen-96 <yakop.96@gmail.com>
2024-11-15 10:51:43 +0200

    done

--- week11/day2/daily-challenge/app.js
+++ week11/day2/daily-challenge/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const quizRouter = require('./routes/quiz');
 
+// parsing to json
 app.use(express.json());
 
+// welcome message 
+app.get('/', (req, res) => {
+    res.send("Welcome to the trivia quiz game Start the game by navigating to /api/quiz/start."); // i always get lost and forget how to start
+});
 
+// using routes
+app.use('/api', quizRouter);
 
 app.listen(PORT, () => {
-    console.log(`trivia running on http://localhost:${PORT}`);
+    console.log(`trivia running on http://localhost:${PORT}!!!!`);
 });
