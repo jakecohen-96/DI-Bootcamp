ab4c0b8e7e7953bd681c55d9db58708f59727c7d
jakecohen-96 <yakop.96@gmail.com>
2024-11-17 19:24:00 +0200

    added menu

--- week15/hackathon-todo-list/index.js
+++ week15/hackathon-todo-list/index.js
@@ -3,6 +3,7 @@ const readline = require('readline-sync');
 
 const filePath = './tasks.json';
 
+// loading tasks from file
 const loadTasks = () => {
     if (!fs.existsSync(filePath)) {
         fs.writeFileSync(filePath, JSON.stringify([]));
@@ -11,10 +12,20 @@ const loadTasks = () => {
     return JSON.parse(data);
 };
 
+// save tasks to file
 const saveTasks = (tasks) => {
     fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2));
 };
 
 const tasks = loadTasks();
 
-console.log('Welcome to your To-Do List');
+const displayMenu = () => {
+    console.log('\nWhat would you like to do?');
+    console.log('1. View Tasks');
+    console.log('2. Add a Task');
+    console.log('3. Exit');
+    const choice = readline.question('Enter your choice: ');
+    return choice;
+};
+
+
