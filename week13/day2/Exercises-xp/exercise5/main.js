23a93a16c159c11e9784b20c4609883f8adeced3
jakecohen-96 <yakop.96@gmail.com>
2024-11-22 14:15:39 +0200

    done

---
+++ week13/day2/Exercises-xp/exercise5/main.js
@@ -0,0 +1,9 @@
+const mathUtils = require('./mathUtils');
+
+const num1 = 10;
+const num2 = 5;
+
+console.log(`Addition: ${mathUtils.add(num1, num2)}`);
+console.log(`Multiplication: ${mathUtils.multiply(num1, num2)}`);
+console.log(`Subtraction: ${mathUtils.subtract(num1, num2)}`);
+console.log(`Division: ${mathUtils.divide(num1, num2)}`);
