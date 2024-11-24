4ea8a5854797714cfa5decb2b481cfbfaabbe3ca
jakecohen-96 <yakop.96@gmail.com>
2024-11-24 18:45:27 +0200

    example of button that fetches data in a json

--- week16/day3/day3-project/components/ExampleClassComponent.jsx
+++ week16/day3/day3-project/components/ExampleClassComponent.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 
 class ExampleClassComponent extends React.Component{
+    fetchUsers = () => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res =>res.json())
+        .then(data => {
+            console.log(data);
+            
+        })
+        .catch(e => {
+            console.log(e);
+            
+        })
+    } 
+
     render(){
         return(
             <>
                 <h2>Example of a class component</h2>
+                <button onClick={this.fetchUsers}>Get Users</button>
             </>
         )
     } 
