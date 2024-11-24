fe1127171bcc9b8588fa198f09bab016696dd584
jakecohen-96 <yakop.96@gmail.com>
2024-11-24 19:07:28 +0200

    example of a constructor

--- week16/day3/day3-project/components/ExampleClassComponent.jsx
+++ week16/day3/day3-project/components/ExampleClassComponent.jsx
@@ -1,27 +1,37 @@
 import React from "react";
 
-class ExampleClassComponent extends React.Component{
-    fetchUsers = () => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(res =>res.json())
-        .then(data => {
-            console.log(data);
-            
-        })
-        .catch(e => {
-            console.log(e);
-            
-        })
-    } 
+class ExampleClassComponent extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            users: [],
+            text: "please click button", // Fixed typo in the default text
+        };
+    }
 
-    render(){
-        return(
+    fetchUsers = () => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((data) => {
+                this.setState({ users: data, text: "changed!" });
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    };
+
+    render() {
+        return (
             <>
                 <h2>Example of a class component</h2>
+                <h2>{this.state.text}</h2> {/* Corrected the reference */}
                 <button onClick={this.fetchUsers}>Get Users</button>
+                {this.state.users.map((item, index) => {
+                    return <div key={index}>{item.name}</div>;
+                })}
             </>
-        )
-    } 
-};
+        );
+    }
+}
 
 export default ExampleClassComponent;
