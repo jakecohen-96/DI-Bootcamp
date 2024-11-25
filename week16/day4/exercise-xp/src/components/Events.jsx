1a785d554150efcb7ff34a98a9b7771131557541
jakecohen-96 <yakop.96@gmail.com>
2024-11-25 21:18:17 +0200

    exercise 2 done

--- week16/day4/exercise-xp/src/components/Events.jsx
+++ week16/day4/exercise-xp/src/components/Events.jsx
@@ -1,15 +1,52 @@
+import { useState } from "react";
+
 const Events = () => {
-    const handleClick = () => {
-        console.log('I was clicked');
-        
-    };
+  const [inputValue, setInputValue] = useState(""); // State for input value
+  const [isToggleOn, setIsToggleOn] = useState(true); // State for toggle button
 
-    return (
-        <div>
-            <button onClick={handleClick}>please click me</button>
-        </div>
-    );
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      alert(`User says: ${inputValue}`);
+    }
+  };
+
+  const handleClick = () => {
+    console.log("I am the first button; I was clicked");
+  };
+
+  const toggleState = () => {
+    setIsToggleOn((prevState) => !prevState);
+  };
+
+  return (
+    <>
+      <div>
+        <button onClick={handleClick}>Please click me</button>
+      </div>
+      <div>
+        <button onClick={() => console.log("Second button; I was clicked")}>
+          Another Click Button
+        </button>
+      </div>
+      <div>
+        <input
+          placeholder="Type something..."
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyPress}
+        />
+      </div>
+      <div style={{ marginTop: "20px" }}>
+        <button onClick={toggleState}>
+          {isToggleOn ? "ON" : "OFF"}
+        </button>
+      </div>
+    </>
+  );
 };
-
 
 export default Events;
