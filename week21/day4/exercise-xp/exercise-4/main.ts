920db1d7d010ea3cbcf34eb7a9e0e1905ac8d716
jakecohen-96 <yakop.96@gmail.com>
2025-01-04 20:55:54 +0200

    4 done

---
+++ week21/day4/exercise-xp/exercise-4/main.ts
@@ -0,0 +1,13 @@
+class Calculator {
+    public static add(a: number, b: number): number {
+        return a + b;
+    }
+    public static subtract(a: number, b: number): number {
+        return a - b;
+    }
+}
+
+console.log(Calculator.add(4,5));
+console.log(Calculator.subtract(10,5));
+
+
