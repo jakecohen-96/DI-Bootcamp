6220fda7e6fde55a49305432f25e702f9d7fd076
jakecohen-96 <yakop.96@gmail.com>
2025-02-17 20:51:23 +0200

    comitting

--- week14/day2/exercise-xp/exercise1/blog-api/config/db.js
+++ week14/day2/exercise-xp/exercise1/blog-api/config/db.js
@@ -1,15 +1,3 @@
-// const { Pool } = require('pg');
-
-// const pool = new Pool({
-//   user: 'jake',
-//   host: 'localhost',
-//   database: 'books_di',
-//   password: 'root',
-//   port: 5432,            
-// });
-
-// module.exports = pool;
-
 const { Pool } = require('pg');
 require('dotenv').config();
 
